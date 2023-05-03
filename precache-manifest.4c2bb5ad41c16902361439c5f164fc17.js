4d07a8b86c01938ff6fc4b0edcea94f756dea158
GunjalAnjali <gunjalanjali128@gmail.com>
2023-05-03 19:24:41 +0530

    Updates

---
+++ precache-manifest.4c2bb5ad41c16902361439c5f164fc17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6bf048ba4fb5067b6b7c",
+    "url": "/anjaligunjal128/EastVantage-DemoProjectt/static/css/main.ed168524.chunk.css"
+  },
+  {
+    "revision": "6bf048ba4fb5067b6b7c",
+    "url": "/anjaligunjal128/EastVantage-DemoProjectt/static/js/main.6bf048ba.chunk.js"
+  },
+  {
+    "revision": "2e76ef4f4b07ab805959",
+    "url": "/anjaligunjal128/EastVantage-DemoProjectt/static/js/1.2e76ef4f.chunk.js"
+  },
+  {
+    "revision": "6b2b2dd257caa4ab907b",
+    "url": "/anjaligunjal128/EastVantage-DemoProjectt/static/js/2.6b2b2dd2.chunk.js"
+  },
+  {
+    "revision": "c1484cc4a1e49f2c9cfe",
+    "url": "/anjaligunjal128/EastVantage-DemoProjectt/static/js/runtime~main.c1484cc4.js"
+  },
+  {
+    "revision": "c26086a027f6f0cdaf4a9586b19f400a",
+    "url": "/anjaligunjal128/EastVantage-DemoProjectt/index.html"
+  }
+];
